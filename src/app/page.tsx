a4d83b2ea3616bcd86d0b28e681ce6e6f3b70566
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2024-04-24 08:25:42 +0200

    logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import {AuthContextProvider} from "@/contexts/AuthContext";
 import dynamic from "next/dynamic";
 import {SelectionPanel} from "@/components/SelectionPanel/SelectionPanel";
 import {ExpirationPanel} from "@/components/ExpirationPanel/ExpirationPanel";
+import {Navbar} from "@/components/Navbar/Navbar";
 
 const AuthModal = dynamic(
     () => import("../components/AuthModal/AuthModal").then((mod) => mod.AuthModal),
@@ -13,12 +14,15 @@ const AuthModal = dynamic(
 
 export default function Home() {
     return (
-        <main className="w-full h-screen bg-slate-100">
+        <main className="w-full h-screen bg-slate-200">
             <AuthContextProvider>
                 <AuthModal/>
-                <div className={'w-full h-full flex gap-8 p-8 bg-slate-200'}>
-                    <SelectionPanel/>
-                    <ExpirationPanel/>
+                <div className={'w-full h-full'}>
+                    <Navbar />
+                    <div className={'w-full h-[calc(100% - 68px)] flex gap-8 p-8'}>
+                        <SelectionPanel/>
+                        <ExpirationPanel/>
+                    </div>
                 </div>
             </AuthContextProvider>
         </main>
